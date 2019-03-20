e67765ab391f6437c4285124a61984ac8516d5e8
MDadash <dadash.rita@gmail.com>
2019-03-20 18:16:25 +0200

    no message

---
+++ wp-content/themes/goldenbuild/footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package goldenbuild
+ */
+
+?>
+
+    <footer>
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-6 footer__contacts">
+		        <a href="tel:74956422334">+7 (495)-642-23-34</a>
+		        <br>
+		        <a href="emailto:">golden.stroy@mail.ru</a>
+		  		</div>
+		    	<div class="col-md-6 text-right">
+			      <div class="social-icons">
+		          <a href="https://www.facebook.com/" class="fab fa-facebook-f"></a>
+		          <a href="https://twitter.com/" class="fab fa-twitter"></a>
+		          <a href="https://plus.google.com/" class="fab fa-google-plus"></a>
+		          <a href="https://instagram.com/" class="fab fa-instagram"></a>
+		      	</div>
+		      	<div class="copyright">
+		      		<p>© 2018, ООО "ГОЛДЕН СТРОЙ"</p>
+		      		<p>Политика конфиденциальности обрабатываемых данных</p>
+		      	</div>
+		    	</div>
+    		</div>		
+			</div>
+    </footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/goldenbuild/index.php
@@ -0,0 +1,299 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package goldenbuild
+ */
+
+get_header();
+?>
+    <main>
+    	<section class="repairs">
+            <div class="container">
+                <div class="row">
+                    <p class="col-12">Наша ремонтно-строительная компания обладает богатым опытом и высоким профессионализмом в области общестроительных, инженерных и отделочных работ. Основной специализацией компании «Голден строй» является реконструкция, ремонт и отделка всех видов коммерческой недвижимости.</p>
+                    <p class="col-md-12 text-center">Мы профессионально выполняем ремонт коммерческих помещенийй в Москве и МО</p>
+                </div>
+            </div>
+    		
+    	</section>
+    	<section class="about">
+    		
+    	</section>
+    	<section class="application">
+    		
+    	</section>
+    	<section class="about">
+            <div class="container">
+                <div class="row">
+                    <h3 class="col-12 my-3">О компании</h3>
+                    <div class="col-md-6">
+                        <p>На протяжении более 5 лет мы занимаемся ремонтными работами коммерческих помещений под ключ. Наши профессионалы берутся за работу любой сложности, от обычного ремонта до капитального, от простой отделки помещений до элитных проектов с тщательной разработкой уникального дизайна. Для “Голден строй” нет невыполнимых решений - мы найдём способ воплотить ваш проект в жизнь и провести ремонт коммерческой недвижимости согласно всем указанным пожеланиям.</p>
+                        <p>Мы прекрасно понимаем, что репутация - наиболее ценный из наших активов, и стараемся соответствовать самым высоким требованиям, предъявленными заказчиками к строительным компаниям.</p>
+                        <p>Мы дорожим своими партнерами, от крупных игроков рынка коммерческой недвижимости до арендаторов малых помещений. Поэтому мы всегда стремимся к долгосрочным отношениям. Мы будем рады видеть вас в числе своих постоянных заказчиков и постараемся оправдать ваше доверие.</p>
+
+                    </div>
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
+            </div>
+    	</section>
+    	<section class="form form-section">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-6">
+    					
+    				</div>
+    				<div class="col-md-6">
+    					форма
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    	<section class="repairs-type container">
+    		<div class="row">
+    			<h3 class="col-12 text-center my-3">Вы можете заказать любой вид ремонта</h3>
+    			<div class="col-md-6 my-3">
+    				<h4>Косметический ремонт</h4>
+    				<img src="<?php bloginfo('template_url') ?>/images/repair.jpg" alt="">
+    				<p>от <span>3000</span> руб/м.кв.</p>
+    			</div>
+    			<div class="col-md-6 my-3">
+    				<ul>
+    					<li>снятие старого настенного покрытия;</li>
+    					<li>шпаклёвка стен, потолков, трещин, неровностей и прочих дефектов;</li>
+    					<li>покраска стен, оклейка обоями от;</li>
+    					<li>монтаж труб отопительных систем;</li>
+    					<li>реставрация внешнего вида отопительных труб;</li>
+    					<li>укладка ламината, паркета, линолеума, ковролина;</li>
+    					<li>обработка и шлифовка паркетных покрытий пола;</li>
+    					<li>установка или замена осветительных приборов;</li>
+    					<li>установка и сборка межкомнатных дверей;</li>
+    					<li>монтаж сантехники.</li>
+    				</ul>
+    			</div>
+    			<div class="col-md-6 my-3">
+    				<ul>
+    					<li>снятие старого настенного покрытия;</li>
+    					<li>шпаклёвка стен, потолков, трещин, неровностей и прочих дефектов;</li>
+    					<li>выравнивание больших повреждённых настенных участков;</li>
+    					<li>покраска стен, оклейка обоями от;</li>
+    					<li>монтаж труб отопительных систем;</li>
+    					<li>реставрация внешнего вида отопительных труб;</li>
+    					<li>укладка ламината, паркета, линолеума, ковролина;</li>
+    					<li>обработка и шлифовка паркетных покрытий пола;</li>
+    					<li>установка или замена осветительных приборов;</li>
+    					<li>установка и сборка межкомнатных дверей;</li>
+    					<li>монтаж сантехники;</li>
+    					<li>электромонтажные работы.</li>
+    				</ul>
+    			</div>
+    			<div class="col-md-6 my-3">
+    				<h4>Капитальный ремонт</h4>
+    				<img src="<?php bloginfo('template_url') ?>/images/repair.jpg" alt="">
+    				<p>от <span>6000</span> руб/м.кв.</p>
+    			</div>
+    			<div class="col-md-6 my-3">
+    				<h4>Элитный ремонт</h4>
+    				<img src="<?php bloginfo('template_url') ?>/images/repair.jpg" alt="">
+    				<p>от <span>14000</span> руб/м.кв.</p>
+    			</div>
+    			<div class="col-md-6 my-3">
+    				<ul>
+    					<li>полный спектр ремонтно-отделочных работ "под ключ";</li>
+    					<li>дизайн-проект объекта;</li>
+    					<li>демонтажные работы;</li>
+    					<li>шпаклёвка стен, потолков, трещин, неровностей и прочих дефектов;</li>
+    					<li>выравнивание больших повреждённых настенных участков;</li>
+    					<li>покраска стен, оклейка обоями и т.д.;</li>
+    					<li>монтаж труб отопительных систем;</li>
+    					<li>монтаж напольного покрытия (ламинат, кладка плитки, ковролин, паркет и т.д.);</li>
+    					<li>установка или замена осветительных приборов;</li>
+    					<li>установка и сборка межкомнатных дверей;</li>
+    					<li>монтаж сантехники;</li>
+    					<li>электромонтажные работы.</li>
+    				</ul>
+    			</div>
+    		</div>
+    		
+    	</section>
+    	<section class="services">
+    		<div class="container">
+    			<div class="row">
+		    		<h3 class="col-12">Наши дополнительные услуги</h3>
+		    		<div class="col-md-6 services__item my-3">
+		    			<img src="<?php bloginfo('template_url') ?>/images/build.jpg" alt="">
+		    			<p class="mx-3 text-center">Общестроительные работы</p>
+		    		</div>
+		    		<div class="col-md-6 services__item my-3">
+		    			<img src="<?php bloginfo('template_url') ?>/images/facade.jpg" alt="">
+		    			<p class="mx-3 text-center">Фасадные работы</p>
+		    		</div>
+		    		<div class="col-md-6 services__item my-3">
+		    			<img src="<?php bloginfo('template_url') ?>/images/roofing.jpg" alt="">
+		    			<p class="mx-3 text-center">Кровельные работы</p>
+		    		</div>
+		    		<div class="col-md-6 services__item my-3">
+		    			<img src="<?php bloginfo('template_url') ?>/images/design.jpg" alt="">
+		    			<p class="mx-3 text-center">Дизайн интерьеров и проектирование</p>
+		    		</div>
+    			</div>
+    		</div>
+    	</section>
+    	<section class="advantages">
+    		<div class="container">
+    			<div class="row">
+	    			<div class="col-md-7">
+	    				<h3 class="col-12">Наши преимущества</h3>
+	    				<ul>
+	    					<li>- большой стаж работы, опотные и квалифицированные сотрудники</li>
+	    					<li>- применение передового оборудования в выполнении любых задач</li>
+	    					<li>- использование современных технологий и регулярная оптимизация рабочего дня позволяет делать всю работу эффективно и качественно</li>
+	    					<li>- выполнение широкого спектра услуг с различной сложностью</li>
+	    					<li>- состовление официального договора</li>
+	    					<li>- гарантия на любую проделаную работу до 5 лет</li>
+	    					<li>- возможность оплаты любым удобным способом, как безналичным так и наличным путем</li>
+	    					<li>- все работы осуществляются в срок, согласно графиков выполнения работ</li>
+	    					<li>- возможность работать в удобное для заказчиков время</li>
+	    				</ul>
+	    			</div>
+	    			<div class="col-md-5">
+	    				<img src="<?php bloginfo('template_url') ?>/images/adv1.jpg" alt="">
+	    				<img src="<?php bloginfo('template_url') ?>/images/adv2.jpg" alt="">
+	    				<img src="<?php bloginfo('template_url') ?>/images/adv3.jpg" alt="">
+	    				<img src="<?php bloginfo('template_url') ?>/images/adv4.jpg" alt="">
+	    			</div>
+    			</div>
+    		</div>
+    	</section>
+    	<section class="contractor">
+    		<div class="container">
+	    		<div class="row">
+	    			<div class="col-12">
+	    				<h3 class="text-center">Как выбрать надежного подрядчика?</h3>
+	    				<p>Опыт фирмы играет важную роль, это первая характеристика на которую стоит обратить особое внимание. Если строительная организация функционирует месяц или несколько недель, рекомендуем воздержаться от такого выбора.</p>
+	    			</div>
+	    			<div class="col-md-8">
+	    				<ol>
+	    					<li>Опыт фирмы играет важную роль, это первая характеристика на которую стоит обратить особое внимание. Если строительная организация функционирует месяц или несколько недель, рекомендуем воздержаться от такого выбора.</li>
+	    					<li>Цены на предоставление услуг не должны быть слишком низкими, это уже наводит на подозрение, что работы будут выполнены некачественно. Ценовая политика должна быть на уровне среднего сегмента. При подписании договорного соглашения внимательно просматривайте все пункты, при оговорке низких цен могут быть дополнительные условия.</li>
+	    					<li>Строительная фирма должна иметь все разрешительные документы на осуществление и предоставление такого рода услуг. Это могут быть сертификаты, лицензии, доказательства прохождения специалистов для повышения квалификации.</li>
+	    					<li>При поиске организаций через сеть Интернет, обязательно ознакомьтесь с отзывами клиентов, просмотрите форумы с обсуждениями. Если есть негативное мнение предшественников, стоит поискать другую компанию.</li>
+	    					<li>Сотрудники компании должны иметь специальную униформу для проведения строительных работ, настоящая фирма, которая имеет конкурентные преимущества на рынке строительства, будет соблюдать технику безопасности.</li>
+	    					<li>Внимательно прочитайте все пункты договора, особенно условия оплаты. Тем самым вы обезопасите себя от наличия подводных камней, которые могут выплывать в уже запущенном процессе строительных работ. Условия и требования должны быть прозрачны, без сомнительного содержания.</li>
+	    				</ol>
+	    			</div>
+	    			<div class="col-md-4">
+	    				<img src="" alt="">
+	    				<img src="" alt="">
+	    				<img src="" alt="">
+	    				<img src="" alt="">
+	    			</div>
+	    			<div class="col-12">
+	    				<p>Строительно-ремонтная компания “Голден Строй” имеет все вышеперечисленные характеристики и готова предоставить квалифицированных строителей для выполнения строительно-монтажных работ. Мы знаем как выбрать надежного подрядчика, и готовы проконсультировать вас по всем вопросам. Услуги выполняются с соблюдением стандартов и норм проектно-технической документации в утвержденные с заказчиком сроки.</p>
+	    			</div>
+	    		</div>
+    		</div>
+    	</section>
+    	<section class="form form-section">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-6">
+    					
+    				</div>
+    				<div class="col-md-6">
+    					форма
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    	<section class="scheme container">
+    		<div class="row">
+                <h3 class="col-12">Схема сотрудничества</h3>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme1.png" alt="">
+    				<p>Звонок/заявка</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme2.png" alt="">
+    				<p>Бесплатный выезд<br> специалиста на объект</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme3.png" alt="">
+    				<p>Выполнение<br> обмерных работ</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme4.png" alt="">
+    				<p>Составление сметы<br> и коммерческого<br> предложения</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme5.png" alt="">
+    				<p>Подписание<br> официального договора</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme6.png" alt="">
+    				<p>Выполнение<br> строительно-монтажных<br> работ</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme7.png" alt="">
+    				<p>Комплектация мебелью<br> и оборудованием</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme8.png" alt="">
+    				<p>Уборка после<br> выполненных работ</p>
+    		    </div>
+    			<div class="col-md-4 text-center">
+    				<img src="<?php bloginfo('template_url') ?>/images/scheme9.png" alt="">
+    				<p>Сдача<br> объекта</p>
+    			</div>
+    		</div>
+    	</section>
+    	<section class="workexamples container">
+            <!-- <div class=""> -->
+                <div class="row">
+                    <h3 class="col-12">Выполненные работы</h3>
+                    <img src="<?php bloginfo('template_url') ?>/images/shop.jpg" alt="" class="col-md-4 my-3">
+                    <img src="<?php bloginfo('template_url') ?>/images/cafe.jpg" alt="" class="col-md-4 my-3">
+                    <img src="<?php bloginfo('template_url') ?>/images/hotel.jpg" alt="" class="col-md-4 my-3">
+                    <img src="<?php bloginfo('template_url') ?>/images/shop.jpg" alt="" class="col-md-4 my-3">
+                    <img src="<?php bloginfo('template_url') ?>/images/cafe.jpg" alt="" class="col-md-4 my-3">
+                    <img src="<?php bloginfo('template_url') ?>/images/hotel.jpg" alt="" class="col-md-4 my-3">    
+                </div>
+            <!-- </div> -->
+            
+    	</section>
+    	<section class="reviews">
+            <div class="container">
+                <div class="row">
+                    <h3>Отзывы и благодарности</h3>
+                </div>
+            </div>
+    		
+    	</section>
+    	<section class="form-section">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-6">
+    					<p>Есть смета конкурентов?</p>
+    					<p>Вам уже составили смету в другой компании?</p>
+    					<p>Отправьте её нам, мы проверим цены и ошибки в расчетах.</p>
+    					<p>Возможно сможем уменьшить стоимость. Аргументируем на чем можно сэкономить и какие позиция выше рыночной стоимости.</p>
+    				</div>
+    				<div class="col-md-6">
+    					форма
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    </main>
+<?php
+
+get_footer();
